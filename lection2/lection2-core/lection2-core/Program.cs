d2580a482d12c36adcd76f7cc345bc64ec1c7a9d
Serhii-Yasenev <serhii.yasenev@ihsmarkit.com>
2020-03-12 21:19:54 +0200

    init lection2

---
+++ lection2/lection2-core/lection2-core/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lection2_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testClassString = new MyClass<string>(5)
+            {
+                // six 
+                "123", "234", "345", "456", "567", "xxx"
+            };
+            var testClassInt = new MyClass<int>(5) { 1, 2, 3, 4, 5 };
+        }
+    }
+
+
+    class MyClass<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private T[] _array;
+        private int _i = -1;
+        private int _arrayIndex = -1;
+
+        public MyClass(int capacity)
+        {
+            _array = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index <= _arrayIndex)
+                {
+                    return _array[index];
+                }
+
+                return default(T);
+            }
+            set
+            {
+                if (index <= _arrayIndex)
+                {
+                    _array[index] = value;  
+                }
+               
+            }
+
+        }
+
+        public void Add (T obj)
+        {
+            _arrayIndex++;
+
+            if (_arrayIndex >= _array.Length)
+            {
+                Array.Resize(ref _array, _array.Length * 2);  
+            }
+
+            _array[_arrayIndex] = obj;
+
+        }
+
+        public bool MoveNext()
+        {
+            _i++;
+            return _i < _array.Length;
+        }
+
+        public void Reset()
+        {
+            _i = -1;
+        }
+
+        object IEnumerator.Current => Current;
+
+        public T Current => _array[_i];
+
+        public IEnumerator<T> GetEnumerator() => this;
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Dispose()
+        {
+            _array = null;
+        }
+    }
+}
---
+++ lection2/lection2/lection2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lection2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // list
+            var list = new List<int>(100);
+
+            // add items
+            for (var i = 0; i < 102; i++)
+            {
+                list.Add(i);
+            }
+
+            var cap = list.Capacity;
+
+
+            // stack
+            // FILO
+            var stack = new Stack<int>(100);
+
+            // add items
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+
+            // get item
+            var firstOut = stack.Peek();
+            var outAndDelete = stack.Pop();
+            var outAgainAndDelete = stack.Pop();
+
+            // stack
+            // FIFO
+            var queue = new Queue<int>(100);
+
+            // add items
+            queue.Enqueue(123);
+            queue.Enqueue(234);
+            queue.Enqueue(345);
+
+
+            // get item
+            var en = queue.GetEnumerator().MoveNext();
+            var justLook = queue.Peek();
+            var first = queue.Dequeue();
+            var second = queue.Dequeue();
+            var third = queue.Dequeue();
+
+
+            //Dictionary
+
+
+            //HashSet
+
+            var hashSet = new HashSet<string>();
+
+            // add items
+            hashSet.Add("test1");
+            hashSet.Add("test2");
+
+            var a = hashSet.Add("test3");
+            var b = hashSet.Add("test1");
+
+            var contains = hashSet.Contains("test2");
+
+
+
+            // counter
+
+            var counter = new Counter();
+
+            foreach (var c in counter)
+            {
+                Console.WriteLine("counter");
+                Console.WriteLine(c);
+            }
+
+            Console.Read();
+
+
+        }
+
+        class Counter : IEnumerator, IEnumerable
+        {
+            private int i = -1;
+
+            public bool MoveNext()
+            {
+                i++;
+                return i < 10;
+            }
+
+            public void Reset()
+            {
+                i = -1;
+            }
+
+            public object Current => i;
+
+            public IEnumerator GetEnumerator() => this;
+        }
+
+        public class Generic<T> where T : unmanaged
+        {
+            public T Field;
+        }
+    }
+}
